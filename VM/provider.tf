b90e8101fbd8b4b0256f10367d00852e6a859c24
Monalidpatil <monali11dpatil@gmail.com>
2024-05-18 17:25:38 +0000

    changes to provider

--- VM/provider.tf
+++ VM/provider.tf
@@ -1,9 +1,8 @@
 terraform {
-  required_version = "1.7.5"
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "4.84.0"
+      version = "5.29.1"
     }
   }
 }
